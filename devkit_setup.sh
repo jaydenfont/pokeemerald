84eb5475c7adb1f7550f51266e729edc228f278d
jaydenfont <jaydenfont@gmail.com>
2023-04-29 20:04:37 -0400

    First change and build

---
+++ devkit_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export DEVKITPRO=/opt/devkitpro
+# echo "export DEVKITPRO=$DEVKITPRO" >> ~/.bashrc
+export DEVKITARM=$DEVKITPRO/devkitARM
+#echo "export DEVKITARM=$DEVKITARM" >> ~/.bashrc
+
+#sudo echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi" >> ~/.bash_profile
+echo "success"
